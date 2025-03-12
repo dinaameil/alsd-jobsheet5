0f1a4ba6eda94fd009c0e0950b32fde7e989afd1
dinaameil <dinaameil753@gmail.com>
2025-03-12 09:06:11 +0700

    mengubah for menjadi while

--- Faktorial.java
+++ Faktorial.java
@@ -1,11 +1,14 @@
 public class Faktorial {
-    int faktorialBF(int n){
+    int faktorialBF(int n) {
         int fakto = 1;
-        for (int i=1; i<=n;i++){
-            fakto = fakto*i;
+        int i = 1;
+        while (i <= n) {
+            fakto *= i;
+            i++;
         }
         return fakto;
     }
+    
     int faktorialDC(int n) {
         if (n==1){
             return 1;
